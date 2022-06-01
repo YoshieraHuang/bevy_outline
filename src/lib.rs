856ded2a266843e68aa9824d5727fab656a2d1a0
Yoshiera <yoshierahuang@126.com>
2022-06-02 00:44:31 +0800

    almost complete

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,13 @@
+mod prepare;
+mod smooth_normal;
+
 use bevy::{
+    core::cast_slice,
     core_pipeline::Transparent3d,
+    ecs::system::{
+        lifetimeless::{Read, SQuery, SRes},
+        SystemParamItem,
+    },
     pbr::{
         DrawMesh, MeshPipeline, MeshPipelineKey, MeshUniform, SetMeshBindGroup,
         SetMeshViewBindGroup,
@@ -7,77 +15,208 @@ use bevy::{
     prelude::*,
     reflect::TypeUuid,
     render::{
-        mesh::MeshVertexBufferLayout,
-        render_asset::RenderAssets,
-        render_phase::{DrawFunctions, RenderPhase, SetItemPipeline, AddRenderCommand},
-        render_resource::{
-            PipelineCache, RenderPipelineDescriptor, SpecializedMeshPipeline,
-            SpecializedMeshPipelineError, SpecializedMeshPipelines, Face,
+        mesh::{MeshVertexAttribute, MeshVertexBufferLayout},
+        render_asset::{PrepareAssetError, RenderAsset, RenderAssetPlugin, RenderAssets},
+        render_component::ExtractComponentPlugin,
+        render_phase::{
+            AddRenderCommand, DrawFunctions, EntityRenderCommand, RenderCommandResult, RenderPhase,
+            SetItemPipeline, TrackedRenderPass,
         },
+        render_resource::{
+            std140::{AsStd140, Std140},
+            BindGroup, BindGroupDescriptor, BindGroupEntry, BindGroupLayout,
+            BindGroupLayoutDescriptor, BindGroupLayoutEntry, BindingType, BlendState, Buffer,
+            BufferBindingType, BufferDescriptor, BufferInitDescriptor, BufferSize, CompareFunction,
+            DepthBiasState, DepthStencilState, Face, FragmentState, FrontFace, MultisampleState,
+            PipelineCache, PolygonMode, PrimitiveState, RenderPipelineDescriptor, ShaderStages,
+            SpecializedMeshPipeline, SpecializedMeshPipelineError, SpecializedMeshPipelines,
+            StencilFaceState, StencilState, TextureFormat, VertexState,
+        },
+        renderer::{RenderDevice, RenderQueue},
+        texture::BevyDefault,
         view::ExtractedView,
         RenderApp, RenderStage,
     },
 };
+use wgpu_types::{BufferUsages, ColorTargetState, ColorWrites, VertexFormat};
+
+use crate::prepare::prepare_outline_mesh;
+
+macro_rules! load_internal_asset {
+    ($app: ident, $handle: ident, $path_str: expr, $loader: expr) => {{
+        let mut assets = $app.world.resource_mut::<bevy::asset::Assets<_>>();
+        assets.set_untracked($handle, ($loader)(include_str!($path_str)));
+    }};
+}
 
 pub const OUTLINE_SHADER_HANDLE: HandleUntyped =
     HandleUntyped::weak_from_u64(Shader::TYPE_UUID, 7053223528096556000);
+
+pub const ATTRIBUTE_OUTLINE_NORMAL: MeshVertexAttribute =
+    MeshVertexAttribute::new("OutlineNormal", 9885409170, VertexFormat::Float32x3);
 
 #[derive(Debug, Default)]
 pub struct OutlinePlugin;
 
 impl Plugin for OutlinePlugin {
     fn build(&self, app: &mut App) {
-        let mut assets = app.world.resource_mut::<Assets<_>>();
-        assets.set_untracked(
+        load_internal_asset!(
+            app,
             OUTLINE_SHADER_HANDLE,
-            Shader::from_wgsl(include_str!("render/outline.wgsl")),
+            "render/outline.wgsl",
+            Shader::from_wgsl
         );
 
-        app.init_resource::<OutlineConfig>();
+        let render_device = app.world.resource::<RenderDevice>();
+        let buffer = render_device.create_buffer(&BufferDescriptor {
+            label: Some("window size uniform buffer"),
+            size: DoubleReciprocalWindowSizeUniform::std140_size_static() as u64,
+            usage: BufferUsages::UNIFORM | BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+
+        app.add_asset::<OutlineMaterial>()
+            .add_plugin(ExtractComponentPlugin::<Handle<OutlineMaterial>>::default())
+            .add_plugin(RenderAssetPlugin::<OutlineMaterial>::default())
+            .add_system_to_stage(CoreStage::PostUpdate, prepare_outline_mesh);
 
         if let Ok(render_app) = app.get_sub_app_mut(RenderApp) {
             render_app
-                .add_render_command::<Transparent3d, DrawOutline>()
+                .add_render_command::<Transparent3d, DrawOutlines>()
+                .insert_resource(DoubleReciprocalWindowSizeMeta {
+                    buffer,
+                    bind_group: None,
+                })
                 .init_resource::<OutlinePipeline>()
                 .init_resource::<SpecializedMeshPipelines<OutlinePipeline>>()
-                .add_system_to_stage(RenderStage::Extract, extract_outline_config)
-                .add_system_to_stage(RenderStage::Extract, extract_outline)
-                .add_system_to_stage(RenderStage::Queue, queue_outlines);
+                .add_system_to_stage(RenderStage::Extract, extract_window_size)
+                .add_system_to_stage(RenderStage::Prepare, prepare_window_size)
+                .add_system_to_stage(RenderStage::Queue, queue_outlines)
+                .add_system_to_stage(RenderStage::Queue, queue_window_size_bind_group);
         }
     }
 }
 
-#[derive(Component, Debug, Clone, Default, Reflect)]
-#[reflect(Component)]
-pub struct Outline;
-
-#[derive(Debug, Clone, Default)]
-pub struct OutlineConfig {
-    global: bool,
+#[derive(TypeUuid, Clone)]
+#[uuid = "f31fac68-fd87-44db-a4c5-eed0bcbb96cd"]
+pub struct OutlineMaterial {
+    pub width: f32,
+    pub color: Color,
 }
 
-fn extract_outline_config(mut commands: Commands, outline_config: Res<OutlineConfig>) {
-    if outline_config.is_added() || outline_config.is_changed() {
-        commands.insert_resource(outline_config.into_inner().clone());
+#[derive(AsStd140)]
+struct OutlineMaterialUniform {
+    width: f32,
+    color: Vec4,
+}
+
+pub struct GpuOutlineMaterial {
+    bind_group: BindGroup,
+}
+
+impl RenderAsset for OutlineMaterial {
+    type ExtractedAsset = OutlineMaterial;
+    type PreparedAsset = GpuOutlineMaterial;
+    type Param = (SRes<RenderDevice>, SRes<OutlinePipeline>);
+
+    fn extract_asset(&self) -> Self::ExtractedAsset {
+        self.clone()
     }
-}
 
-fn extract_outline(mut commands: Commands, query: Query<Entity, With<Outline>>) {
-    for entity in query.iter() {
-        commands.get_or_spawn(entity).insert(Outline);
+    fn prepare_asset(
+        extracted_asset: Self::ExtractedAsset,
+        (render_device, pipeline): &mut SystemParamItem<Self::Param>,
+    ) -> Result<Self::PreparedAsset, PrepareAssetError<Self::ExtractedAsset>> {
+        let uniform = OutlineMaterialUniform {
+            width: extracted_asset.width,
+            color: extracted_asset.color.as_linear_rgba_f32().into(),
+        };
+
+        let buffer = render_device.create_buffer_with_data(&BufferInitDescriptor {
+            label: None,
+            contents: uniform.as_std140().as_bytes(),
+            usage: BufferUsages::UNIFORM | BufferUsages::COPY_DST,
+        });
+
+        let bind_group = render_device.create_bind_group(&BindGroupDescriptor {
+            label: None,
+            layout: &pipeline.material_layout,
+            entries: &[BindGroupEntry {
+                binding: 0,
+                resource: buffer.as_entire_binding(),
+            }],
+        });
+        Ok(GpuOutlineMaterial { bind_group })
     }
 }
 
 pub struct OutlinePipeline {
-    mesh_pipeline: MeshPipeline,
-    shader: Handle<Shader>,
+    pub mesh_layout: BindGroupLayout,
+    pub view_layout: BindGroupLayout,
+    pub material_layout: BindGroupLayout,
+    pub window_size_layout: BindGroupLayout,
 }
 
 impl FromWorld for OutlinePipeline {
     fn from_world(render_world: &mut World) -> Self {
+        let mesh_pipeline = render_world.resource::<MeshPipeline>();
+        let render_device = render_world.resource::<RenderDevice>();
+        let mesh_binding = BindGroupLayoutEntry {
+            binding: 0,
+            visibility: ShaderStages::VERTEX | ShaderStages::FRAGMENT,
+            ty: BindingType::Buffer {
+                ty: BufferBindingType::Uniform,
+                has_dynamic_offset: true,
+                min_binding_size: BufferSize::new(MeshUniform::std140_size_static() as u64),
+            },
+            count: None,
+        };
+
+        let mesh_layout = render_device.create_bind_group_layout(&BindGroupLayoutDescriptor {
+            entries: &[mesh_binding],
+            label: Some("mesh_layout"),
+        });
+
+        let view_layout = mesh_pipeline.view_layout.clone();
+
+        let material_layout = render_device.create_bind_group_layout(&BindGroupLayoutDescriptor {
+            label: Some("material layout"),
+            entries: &[BindGroupLayoutEntry {
+                binding: 0,
+                visibility: ShaderStages::VERTEX | ShaderStages::FRAGMENT,
+                ty: BindingType::Buffer {
+                    ty: BufferBindingType::Uniform,
+                    has_dynamic_offset: false,
+                    min_binding_size: BufferSize::new(
+                        OutlineMaterialUniform::std140_size_static() as u64
+                    ),
+                },
+                count: None,
+            }],
+        });
+
+        let window_size_layout =
+            render_device.create_bind_group_layout(&BindGroupLayoutDescriptor {
+                label: Some("window size layout"),
+                entries: &[BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: ShaderStages::VERTEX,
+                    ty: BindingType::Buffer {
+                        ty: BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: BufferSize::new(
+                            DoubleReciprocalWindowSizeUniform::std140_size_static() as u64,
+                        ),
+                    },
+                    count: None,
+                }],
+            });
+
         Self {
-            mesh_pipeline: render_world.resource::<MeshPipeline>().clone(),
-            shader: OUTLINE_SHADER_HANDLE.typed(),
+            mesh_layout,
+            view_layout,
+            material_layout,
+            window_size_layout,
         }
     }
 }
@@ -90,37 +229,97 @@ impl SpecializedMeshPipeline for OutlinePipeline {
         key: Self::Key,
         layout: &MeshVertexBufferLayout,
     ) -> Result<RenderPipelineDescriptor, SpecializedMeshPipelineError> {
-        let mut descriptor = self.mesh_pipeline.specialize(key, layout)?;
-        descriptor.vertex.shader = self.shader.clone_weak();
-        descriptor.fragment.as_mut().unwrap().shader = self.shader.clone_weak();
-        descriptor.primitive.cull_mode = Some(Face::Front);
-        Ok(descriptor)
+        let vertex_attributes = vec![
+            Mesh::ATTRIBUTE_POSITION.at_shader_location(0),
+            ATTRIBUTE_OUTLINE_NORMAL.at_shader_location(1),
+        ];
+
+        let bind_group_layout = vec![
+            self.view_layout.clone(),
+            self.mesh_layout.clone(),
+            self.material_layout.clone(),
+            self.window_size_layout.clone(),
+        ];
+
+        let vertex_buffer_layout = layout.get_layout(&vertex_attributes)?;
+
+        Ok(RenderPipelineDescriptor {
+            vertex: VertexState {
+                shader: OUTLINE_SHADER_HANDLE.typed::<Shader>(),
+                entry_point: "vertex".into(),
+                shader_defs: vec![],
+                buffers: vec![vertex_buffer_layout],
+            },
+            fragment: Some(FragmentState {
+                shader: OUTLINE_SHADER_HANDLE.typed::<Shader>(),
+                shader_defs: vec![],
+                entry_point: "fragment".into(),
+                targets: vec![ColorTargetState {
+                    format: TextureFormat::bevy_default(),
+                    blend: Some(BlendState::REPLACE),
+                    write_mask: ColorWrites::ALL,
+                }],
+            }),
+            layout: Some(bind_group_layout),
+            primitive: PrimitiveState {
+                front_face: FrontFace::Ccw,
+                cull_mode: Some(Face::Front),
+                unclipped_depth: false,
+                polygon_mode: PolygonMode::Fill,
+                conservative: false,
+                topology: key.primitive_topology(),
+                strip_index_format: None,
+            },
+            depth_stencil: Some(DepthStencilState {
+                format: TextureFormat::Depth32Float,
+                depth_write_enabled: false,
+                depth_compare: CompareFunction::Greater,
+                stencil: StencilState {
+                    front: StencilFaceState::IGNORE,
+                    back: StencilFaceState::IGNORE,
+                    read_mask: 0,
+                    write_mask: 0,
+                },
+                bias: DepthBiasState {
+                    constant: 0,
+                    slope_scale: 0.0,
+                    clamp: 0.0,
+                },
+            }),
+            multisample: MultisampleState {
+                count: key.msaa_samples(),
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            label: Some("outline_mesh_pipeline".into()),
+        })
     }
 }
 
+#[allow(clippy::too_many_arguments)]
+#[allow(clippy::type_complexity)]
 fn queue_outlines(
-    opaque_3d_draw_functions: Res<DrawFunctions<Transparent3d>>,
+    transparent_3d_draw_functions: Res<DrawFunctions<Transparent3d>>,
     render_meshes: Res<RenderAssets<Mesh>>,
-    // outline_config: Res<OutlineConfig>,
     outline_pipeline: Res<OutlinePipeline>,
     mut pipelines: ResMut<SpecializedMeshPipelines<OutlinePipeline>>,
     mut pipeline_cache: ResMut<PipelineCache>,
     msaa: Res<Msaa>,
-    mut material_meshes: Query<(Entity, &Handle<Mesh>, &MeshUniform), With<Outline>>,
+    material_meshes: Query<(Entity, &Handle<Mesh>, &MeshUniform), With<Handle<OutlineMaterial>>>,
     mut views: Query<(&ExtractedView, &mut RenderPhase<Transparent3d>)>,
 ) {
-    let draw_custom = opaque_3d_draw_functions
+    let draw_function = transparent_3d_draw_functions
         .read()
-        .get_id::<DrawOutline>()
+        .get_id::<DrawOutlines>()
         .unwrap();
 
     let msaa_key = MeshPipelineKey::from_msaa_samples(msaa.samples);
 
-    for (view, mut opaque_phase) in views.iter_mut() {
+    for (view, mut transparent_phase) in views.iter_mut() {
         let view_matrix = view.transform.compute_matrix();
         let view_row_2 = view_matrix.row(2);
 
-        for (entity, mesh_handle, mesh_uniform) in material_meshes.iter_mut() {
+        for (entity, mesh_handle, mesh_uniform) in material_meshes.iter() {
             if let Some(mesh) = render_meshes.get(mesh_handle) {
                 let key =
                     msaa_key | MeshPipelineKey::from_primitive_topology(mesh.primitive_topology);
@@ -133,10 +332,10 @@ fn queue_outlines(
                         return;
                     }
                 };
-                opaque_phase.add(Transparent3d {
+                transparent_phase.add(Transparent3d {
                     entity,
                     pipeline,
-                    draw_function: draw_custom,
+                    draw_function,
                     distance: view_row_2.dot(mesh_uniform.transform.col(3)),
                 })
             }
@@ -144,9 +343,106 @@ fn queue_outlines(
     }
 }
 
-type DrawOutline = (
+type DrawOutlines = (
     SetItemPipeline,
     SetMeshViewBindGroup<0>,
     SetMeshBindGroup<1>,
+    SetOutlineMaterialBindGroup<2>,
+    SetWindowSizeBindGroup<3>,
     DrawMesh,
 );
+
+pub struct SetOutlineMaterialBindGroup<const I: usize>;
+impl<const I: usize> EntityRenderCommand for SetOutlineMaterialBindGroup<I> {
+    type Param = (
+        SRes<RenderAssets<OutlineMaterial>>,
+        SQuery<Read<Handle<OutlineMaterial>>>,
+    );
+    fn render<'w>(
+        _view: Entity,
+        item: Entity,
+        (materials, query): SystemParamItem<'w, '_, Self::Param>,
+        pass: &mut TrackedRenderPass<'w>,
+    ) -> RenderCommandResult {
+        let material_handle = query.get(item).unwrap();
+        let material = materials.into_inner().get(material_handle).unwrap();
+        pass.set_bind_group(I, &material.bind_group, &[]);
+        RenderCommandResult::Success
+    }
+}
+
+pub struct SetWindowSizeBindGroup<const I: usize>;
+impl<const I: usize> EntityRenderCommand for SetWindowSizeBindGroup<I> {
+    type Param = SRes<DoubleReciprocalWindowSizeMeta>;
+
+    fn render<'w>(
+        _view: Entity,
+        _item: Entity,
+        window_size: SystemParamItem<'w, '_, Self::Param>,
+        pass: &mut TrackedRenderPass<'w>,
+    ) -> RenderCommandResult {
+        let window_size_bind_group = window_size.into_inner().bind_group.as_ref().unwrap();
+        pass.set_bind_group(I, window_size_bind_group, &[]);
+
+        RenderCommandResult::Success
+    }
+}
+
+pub struct ExtractedWindowSize {
+    width: f32,
+    height: f32,
+}
+
+#[derive(AsStd140)]
+pub struct DoubleReciprocalWindowSizeUniform {
+    size: Vec2,
+}
+
+pub struct DoubleReciprocalWindowSizeMeta {
+    buffer: Buffer,
+    bind_group: Option<BindGroup>,
+}
+
+fn extract_window_size(mut commands: Commands, windows: Res<Windows>) {
+    if windows.is_added() || windows.is_changed() {
+        let window = windows.get_primary().unwrap();
+        let width = window.width();
+        let height = window.height();
+        commands.insert_resource(ExtractedWindowSize { width, height });
+    }
+}
+
+fn prepare_window_size(
+    window_size: Res<ExtractedWindowSize>,
+    window_size_meta: ResMut<DoubleReciprocalWindowSizeMeta>,
+    render_queue: Res<RenderQueue>,
+) {
+    if window_size.is_added() || window_size.is_changed() || window_size_meta.is_changed() {
+        let window_size_uniform = DoubleReciprocalWindowSizeUniform {
+            size: Vec2::new(2.0 / window_size.width, 2.0 / window_size.height),
+        };
+        render_queue.write_buffer(
+            &window_size_meta.buffer,
+            0,
+            cast_slice(&[window_size_uniform.size]),
+        )
+    }
+}
+
+fn queue_window_size_bind_group(
+    render_device: Res<RenderDevice>,
+    mut double_reciprocal_window_size_meta: ResMut<DoubleReciprocalWindowSizeMeta>,
+    pipeline: Res<OutlinePipeline>,
+) {
+    let bind_group = render_device.create_bind_group(&BindGroupDescriptor {
+        label: Some("window size bind group"),
+        layout: &pipeline.window_size_layout,
+        entries: &[BindGroupEntry {
+            binding: 0,
+            resource: double_reciprocal_window_size_meta
+                .buffer
+                .as_entire_binding(),
+        }],
+    });
+    double_reciprocal_window_size_meta.bind_group = Some(bind_group);
+}
